a6355cee73ed4a78708c4229fdea7ee741a2c165
Pauline122112 <pauline.design007@gmail.com>
2021-07-16 15:23:53 -0500

    added style-component: FINALLY

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,13 +1,25 @@
 import React from 'react'
+import styled, { keyframes } from 'styled-components'
+
+
+const StyledCharacter= styled.button`
+border: 2px solid black;
+width: 15%;
+margin-left: 1%;
+padding: 15px;
+font-weight: normal;
+opacity: 0.7;
+`
 
 
 export default function CharactersDetails({info, action}) {
     return (
        
-        <button onClick={() => action(info)}>
-            {info.name}<br></br><div>Reveal</div>
-        </button>
-  
+        <>
+        <StyledCharacter onClick={() => action(info)}>
+            {info.name}<div></div>
+        </StyledCharacter>
+        </>
     )
     }   
     
