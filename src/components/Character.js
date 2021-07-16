65e6eab6dfabec8f60a47876b8cdb067f196623c
Pauline122112 <pauline.design007@gmail.com>
2021-07-16 16:46:05 -0500

    playing around with CSS

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import styled, { keyframes } from 'styled-components'
 
-
+//add css styles
 const StyledCharacter= styled.button`
-border: 2px solid black;
+
+border-radius: 3px;
+border: 2px solid darkbrown;
 width: 15%;
 margin-left: 1%;
 padding: 15px;
 font-weight: normal;
 opacity: 0.7;
 &:hover{
-     transform: scale(1.1);
-     `
+transform: scale(1.1);
+}
+`
 
 
 
--- src/components/CharacterDetails.js
+++ src/components/CharacterDetails.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const JediMindTricks = styled.div`
+color:black;
+display:block;
+background-color: lightgrey;
+opacity: 0.7;
+text-align:center;
+margin:20%;
+align-items: center;
+border:3px solid lightgray;
+padding:0px 20px;
+font-weight: bold;
+font-size: 20px;
+`
 
 export default function JediPowers(props) {
     const { jediInfo, close } = props
  return(
-        <div>
+        <JediMindTricks>
             <h3>Details of {jediInfo.name}</h3>
                 <p>Gender: {jediInfo.gender}</p>
                 <p>Height: {jediInfo.height}</p>
@@ -12,8 +27,8 @@ export default function JediPowers(props) {
                 <p>Eye Color: {jediInfo.eye_color}</p>
                 <p>Hair Color: {jediInfo.hair_color}</p>
                 <p>Skin Color: {jediInfo.skin_color}</p>
-            <buttom onClick={close}>Close</buttom>
-        </div>
+            <button onClick={close}>Close</button>
+        </JediMindTricks>
         )
 
 
